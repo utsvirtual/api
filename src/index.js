a8c5c6d955288d877f501f270d6a7637b469f326
UVirtual <125402400+utsvirtual@users.noreply.github.com>
2023-03-09 09:19:10 -0500

    cambio puerto
    cambio puerto pruebas

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ try {
 // routes
 app.use(require('./routes/index.js'));
 
-const PORT = process.env.PORT || 4000
+const PORT = 443//process.env.PORT || 4000
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`);
 });
